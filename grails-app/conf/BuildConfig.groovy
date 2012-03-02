7ff92e16dbfd55b6e4f0a14ba877543ff77906d4
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-03-02 14:20:03 -0600

    configuring customized plugin to publish to local artifactory server

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,12 @@ grails.project.work.dir = 'target'
 
 grails.project.docs.output.dir = "docs"
 
+// Plugin Publishing Repository Information
+grails.project.repos.umnNcs.url = "http://artifact.ncs.umn.edu/plugins-release-local"
+grails.project.repos.umnNcs.type = "maven"
+grails.project.repos.default = "umnNcs"
+grails.release.scm.enabled = false
+
 grails.project.dependency.resolution = {
 
     inherits "global"
